f30f3656219480b5678d5e74e62de3afe1d28bd8
김수빈 <ambitiousksb@gmail.com>
2024-03-16 23:14:46 +0900

    Project 'restful' Start

---
+++ src/main/java/com/ksb/restful/RestfulApplication.java
@@ -0,0 +1,19 @@
+package com.ksb.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class RestfulApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ac = SpringApplication.run(RestfulApplication.class, args);
+
+		String[] allBeanNames = ac.getBeanDefinitionNames();
+
+		for (String beanName : allBeanNames) {
+			System.out.println(beanName);
+		}
+	}
+}
