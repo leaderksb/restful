8b4e79e419f4a6432e37ecf780e7f9abbd9b2e26
김수빈 <ambitiousksb@gmail.com>
2024-04-06 19:38:46 +0900

    Feat : React 개발 환경 Test

--- src/main/reactjs/src/setProxy.js
+++ src/main/reactjs/src/setProxy.js
@@ -1,10 +1,10 @@
 const {createProxyMiddleware} = require('http-proxy-middleware')
 
 module.exports = app => {
-    app.use('/proxy',
+    app.use('/',
         createProxyMiddleware(
             {
-                target: 'http://localhost:10000',
+                target: 'http://localhost:9000',
                 changeOrigin: true,
             }
         )
