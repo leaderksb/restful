90bac4ba415087bc079e54a3961fce7e5ca4ce2f
김수빈 <ambitiousksb@gmail.com>
2024-04-06 19:38:05 +0900

    Feat : Spring Boot Port 12000 설정, React Port 9000 설정

--- src/main/java/com/ksb/restful/controller/HelloWorldController.java
+++ src/main/java/com/ksb/restful/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.ksb.restful.controller;
 
 import com.ksb.restful.controller.bean.HelloWorldBean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +28,36 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean/path-variable/{name}")
     public HelloWorldBean HelloWorldBeanPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World %s", name));
+    }
+
+    @GetMapping(value = {"", "/"})
+    public String index() {
+        System.out.println("index");
+        return "index";
+    }
+
+    @GetMapping(value = "/not-cors")
+    public String notCors() {
+        System.out.println("not-cors");
+        return "notCors";
+    }
+
+    @CrossOrigin("http://localhost:9000")
+    @GetMapping(value = "/cors")
+    public String cors() {
+        System.out.println("cors");
+        return "cors";
+    }
+
+    @GetMapping(value = "/not-proxy")
+    public String notProxy() {
+        System.out.println("not-proxy");
+        return "notProxy";
+    }
+
+    @GetMapping(value = "/proxy")
+    public String proxy() {
+        System.out.println("proxy");
+        return "proxy";
     }
 }
